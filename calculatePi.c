1c41497a4a383f1cf41622681f08bbc72f7fb8ea
jannikg <jannikg@student.ethz.ch>
2018-10-07 12:33:45 +0200

    added all coeffs

--- calculatePi.c
+++ calculatePi.c
@@ -19,6 +19,11 @@ int main (int argc, char **argv) {
     mpfr_inits(t1, t2, t3, t4, t5, t6, t7, PI, one, NULL);
     mpfr_set_ui(PI, 0, MPFR_RNDN);
     mpfr_set_ui(one, 1, MPFR_RNDN);
+
+    for (size_t i = 0; i < 7; i++) {
+      calculate_correct_Term (i);
+    }
+
     return (0);
 }
 
@@ -38,4 +43,41 @@ void calc(mpfr_t* t, char* x_str, int op, int coeff) {
 
     mpfr_clears(*t, x, NULL);
     mpfr_free_cache();
+}
+
+void calculate_correct_Term (int id) {
+  char x_str[22];
+
+  switch (id) {
+    case 0:
+          sprintf(x_str, "239");
+          calc(&t1, x_str, 1, 183);
+          break;
+    case 1:
+          sprintf(x_str, "1023");
+          calc(&t2, x_str, 1, 32);
+          break;
+    case 2:
+          sprintf(x_str, "5832");
+          calc(&t3, x_str, 2, 68);
+          break;
+    case 3:
+          sprintf(x_str, "113021");
+          calc(&t4, x_str, 1, 12);
+          break;
+    case 4:
+          sprintf(x_str, "6826318");
+          calc(&t5, x_str, 2, 100);
+          break;
+    case 5:
+          sprintf(x_str, "33366019650");
+          calc(&t6, x_str, 2, 12);
+          break;
+    case 6:
+          sprintf(x_str, "43599522992503626068");
+          calc(&t7, x_str, 1, 12);
+          break;
+   default:
+          break;
+  }
 }
