6d7b9c1a3b8e598c96781f4377e5512ab3581952
jannikg <jannikg@student.ethz.ch>
2018-10-07 11:58:42 +0200

    added all global variables

--- calculatePi.c
+++ calculatePi.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
-#include"mpfr.h"
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
 
-int main () {
-    int x = ADD;
-    printf("%d\n", x);
+#include"mpfr.h"
+#include<pthread.h>
+
+unsigned long PRECISION = 100;
+mpfr_t PI, one;
+
+time_t t_start, t_end;
+
+int main (int argc, char **argv) {
+
+    PRECISION *= 3.35;
+    mpfr_set_default_prec(PRECISION);
+    mpfr_inits(PI, one);
+    mpfr_set_ui(pi, 0, MPFR_RNDN);
+    mpfr_set_ui(one, 1, MPFR_RNDN);
     return (0);
+}
+
+
+
 }
